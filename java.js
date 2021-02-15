a688c2313625fd83c21384befe49248604f3d862
SebasGi <70275248+SebasGi@users.noreply.github.com>
2021-02-15 00:58:28 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,39 @@
+var vec=["Marta","Samuel","Juliana","Sebastian"];
+console.log(vec);
+
+var si=function()
+{
+	var i=0;
+	var nom=document.getElementById("nom").value;
+	var ul=vec.length-1; 
+	while(i<vec.length)
+	{
+		if(nom==vec[i])
+		{
+			var ult=vec[ul];
+			vec[ul]=vec[i];
+			vec[i]=ult;
+			vec.pop();
+			console.log(vec);
+			i=vec.length;
+			document.getElementById("re").value="NOMBRE ELIMINADO";
+			document.getElementById("nom").value="";
+		}
+		else
+		{
+			i=i+1;
+			if (nom!=vec[i])
+			{
+				document.getElementById("re").value=nom+" NO SE ENCUENTRA";
+			}
+		}
+	}
+}
+
+var ing=function()
+{
+	var nombi=document.getElementById("nombi").value;
+	vec.push(nombi);
+	console.log(vec);
+	document.getElementById("nombi").value="";
+}
